626569608313aeea91291cfdfcf6757705849b4a
Dylan Morgan <dylanparkermorgan@gmail.com>
2016-03-14 14:43:02 -0400

    Heavily changed how cwdmModel saves/stores models

--- cwdmModel.py
+++ cwdmModel.py
@@ -6,11 +6,11 @@ try:
 except:
     import pickle
 
+import itertools
 import sklearn.base
 
-
 class cwdmModel(sklearn.base.BaseEstimator, sklearn.base.ClassifierMixin):
-    def __init__(self, X_train, y_train, X_test, y_test, model=None):
+    def __init__(self,model=None, bands=None, model_params = {}):
 
         assert hasattr(model, "fit")
         assert hasattr(model, "predict")
@@ -20,19 +20,50 @@ class cwdmModel(sklearn.base.BaseEstimator, sklearn.base.ClassifierMixin):
         else:
             assert hasattr(model, "predict_proba")
 
-        # Store training data as attributes. Not the most efficient way...
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
-        self.y_test = y_test
-
-        # Set Attributes
-        self.clf = sklearn.base.clone(model)
-        self.trained = None
+        self.model = model.set_params(**model_params)
+        self.bands = bands
+        self.colors = self.get_color_permutations(bands)
+        self._trained_models = dict()
+        self._trained_models_scores = dict()
 
     def fit(self, X, y):
-        self.clf.fit(X,y)
-        self.trained = True
+        #trained_models = dict()
+        for each in self.colors:
+            X_color = self.get_color_indices(X, each)
+            clf = sklearn.base.clone(self.model)
+            clf.fit(X_color, y)
+            # Score serves as classifier
+            score = int(100*clf.score(X_color, y))
+            self._trained_models[each] = pickle.dumps(clf)
+            self._trained_models_scores[each] = score
+        return self
 
     def predict(self, X, y=None):
-        return self.clf.predict(X)
+        predictions = []
+        weights = []
+        for color, model_string in self._trained_models.items():
+        	clf = pickle.loads(model_string)
+        	pred = clf.predict(X)
+        	weight = self._trained_models_score[color]
+        	weights.append(weight)
+        	predictions.append(pred * weight)
+
+        predictions = np.array(predictions) / np.sum(weights)
+        y_pred = np.mean(predictions, axis=0)
+        return y_pred
+
+    def get_color_permutations(self, bands):
+        # Color comboinations
+        c_combo = list(itertools.combinations(bands,2))
+        # Color-cut combinations
+        cc_combo = list(itertools.combinations(c_combo,2))
+        cc_labels = ["".join([cc[0][0],cc[0][1],cc[1][0],cc[1][1]]) for cc in cc_combo]
+
+        return cc_labels
+
+    def get_color_indices(self, X, color):
+        # Make X from colors
+        color_x = np.array(X[color[2]] - X[color[3]])
+        color_y = np.array(X[color[0]] - X[color[1]])
+
+        return np.vstack([color_x,color_y]).T
--- cwdm_ML.py
+++ cwdm_ML.py
@@ -27,9 +27,10 @@ from sklearn.cross_validation import ShuffleSplit, StratifiedShuffleSplit
 global localpath
 localpath = '/Users/dpmorg/gdrive/research/cwdm_ML/'
 
-def test_classifiers():
-    TRAINING = 'ugrizTraining.fits'
+global TRAINING
+TRAINING = 'ugrizTraining.fits'
 
+def test_classifiers(N=250):
     # Load training data -- set up as pandas data frame.
     training = fileprep(TRAINING)
 
@@ -42,65 +43,35 @@ def test_classifiers():
 
     # Setting up the color-color combinations
     filters = 'ugriz'
-    color_combinations = list(itertools.combinations(filters,2))
-    color_cut_combinations = list(itertools.combinations(color_combinations,2))
-    # Unpack color labels
-    color_labels = ["".join([cc[0][0],cc[0][1],cc[1][0],cc[1][1]]) for cc in color_cut_combinations]
 
-    # Training and test size
-    N = 250
-    # Select randomly from training and testing
+    # Select N (default=250) random objects from training file.
     y = training["iscwdm"]
     sss = StratifiedShuffleSplit(y, n_iter=1, test_size=N, train_size=N)
     trn_ind, tst_ind = tuple(sss)[0]
 
     # Make sure train/test are the same across all colors and classifiers
-    train_smp = training.iloc[trn_ind]
-    test_smp = training.iloc[tst_ind]
+    X_train = training.iloc[trn_ind]
+    y_train = training["iscwdm"].iloc[trn_ind]
 
-    fits = pd.DataFrame(index=color_labels, columns=classifier_names)
-    scores = pd.DataFrame(index=color_labels, columns=classifier_names)
+    # Create dictionary to store fits
+    fits = {}
 
     # Train a model for each color_cut_combination for each classifier being used.
     for name,clf in zip(classifier_names,classifiers):
         # Initialize the classifier
         # Train the classifier on each color cut
-        for label, color_cut in zip(color_labels,color_cut_combinations):
-            # Pull out the colors from the training
-            trn_x = np.array(train_smp[color_cut[1][0]]-
-                             train_smp[color_cut[1][1]])
-            trn_y = np.array(train_smp[color_cut[0][0]]-
-                             train_smp[color_cut[0][1]])
+        cwdm = cwdmModel(model=clf, bands=filters)
+        cwdm_trained = cwdm.fit(X_train,y_train)
 
-            X_trn, y_trn = np.vstack([trn_x,trn_y]).T, train_smp['iscwdm']
+        # Save model to dictionary
+        fits[name] = cwdm_trained
 
-            # Pull out the colors from the testing
-            tst_x = np.array(test_smp[color_cut[1][0]]-
-                             test_smp[color_cut[1][1]])
-            tst_y = np.array(test_smp[color_cut[0][0]]-
-                             test_smp[color_cut[0][1]])
-
-            X_tst, y_tst = np.vstack([tst_x,tst_y]).T, test_smp['iscwdm']
-
-            # Initiate the model
-            cwdm = cwdmModel(X_trn, y_trn, X_tst, y_tst, model=clf)
-
-            # Train the classifier and get the score
-            cwdm.fit(X_trn, y_trn)
-            score = cwdm.score(X_tst, y_tst)
-
-            fits.loc[label,name] = cwdm
-            scores.loc[label,name] = score
-
-    # Combine fits and scores
-    panel = pd.Panel({"fits" : fits, "scores" : scores})
-
-    # Dump everything into a file
+    # Pickle dump
     the_time = str(time.time())
     filename = "".join([localpath,"data/","trained_models-", the_time, ".pkl"])
     print("\nINFO: Writing results to %s\n" % filename)
     with open(filename, "w") as f:
-        pickle.dump(panel, f)
+        pickle.dump(fits, f)
 
 def plot_all_classifiers(filename=None):
     rcParams.update({'figure.autolayout': True})
@@ -114,101 +85,90 @@ def plot_all_classifiers(filename=None):
     pkl_file = open(filename, 'rb')
     trained_models = pickle.load(pkl_file)
 
-    # Grab row and column names
-    color_cut_combinations = trained_models['fits'].index
-    classifiers = trained_models['fits'].columns
+    #####################
+    # Set up all the data
+    # Grab classifiers names and add "data" at the front.
+    classifier_names = np.append("data",trained_models.keys())
+    # Load training data -- set up as pandas data frame.
+    training = fileprep(TRAINING)
+    # Select testing sample from the training data.
+    y = training["iscwdm"]
+    sss = StratifiedShuffleSplit(y, n_iter=1, test_size=150)
+    trn_ind, tst_ind = tuple(sss)[0]
+    # Make sure train/test are the same across all colors and classifiers
+    X_test = training.iloc[tst_ind]
+    y = training["iscwdm"].iloc[tst_ind]
 
+    # Get the colors from one of the classifiers
+    colors = trained_models[classifier_names[1]].colors
+
+    #
     # Setting up plotting variables
-    ncols = len(classifiers)+1
-    nrows = len(color_cut_combinations)
     rows_per_page = 4
-    npages = int(round(nrows/rows_per_page))
+    ncols = len(classifier_names)
 
+    ########################
+    # Set up plotting region
+    # Colors
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
     # Open pdf
     pdf = PdfPages(localpath+"plots/all_ugriz_classifiers.pdf")
-
     # Set the first first figure. Will need to close it and re-initialize once
     # we plot rows_per_page
-    figure = plt.figure(figsize=((len(classifiers)+1)*3,rows_per_page*3))
+    figure = plt.figure(figsize=(ncols*3,rows_per_page*3))
     i, j = 1, 1 # plot_num
 
-    for color_cut in color_cut_combinations:
-        # Just plot the data set first
-        data = trained_models['fits'].loc[color_cut,classifiers[0]]
+    for each in colors:
+        # Get X for the specified color
+        color_x = np.array(X_test[each[2]] - X_test[each[3]])
+        color_y = np.array(X_test[each[0]] - X_test[each[1]])
+        X = np.vstack([color_x,color_y]).T
 
-        X_train = data.X_train
-        y_train = data.y_train
-        X_test = data.X_test
-        y_test = data.y_test
-
-        # Set-up mesh grid
-        x_min, x_max = X_train[:, 0].min() - .4, X_train[:, 0].max() + .4
-        y_min, y_max = X_train[:, 1].min() - .4, X_train[:, 1].max() + .4
+        # Set-up the mesh grid
+        x_min, x_max = X[:, 0].min() - .4, X[:, 0].max() + .4
+        y_min, y_max = X[:, 1].min() - .4, X[:, 1].max() + .4
         h = 0.02 #mesh grid step size
 
         xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                              np.arange(y_min, y_max, h))
 
-        # just plot the dataset first
-        cm = plt.cm.RdBu
-        cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-        print i, color_cut
-        ax = plt.subplot(rows_per_page, len(classifiers) + 1, i)
-        # Plot the training points
-        ax.scatter(X_train[:, 0], X_train[:, 1], s=20, c=y_train, cmap=cm_bright)
-        # and testing points
-        ax.scatter(X_test[:, 0], X_test[:, 1], s=40, c=y_test, cmap=cm_bright, alpha=0.6)
-        ax.set_xlim(xx.min(), xx.max())
-        ax.set_ylim(yy.min(), yy.max())
-        ax.set_xlabel("".join([color_cut[2],"-",color_cut[3]]),fontsize=16)
-        ax.set_ylabel("".join([color_cut[0],"-",color_cut[1]]),fontsize=16)
 
-        # Increment plot number
-        i += 1
+        for name in classifier_names:
+            # Just plot the data first
+            print i, each, name
+            ax = plt.subplot(rows_per_page, ncols, i)
 
-        for name in classifiers:
-            print i, color_cut, name
-            ax = plt.subplot(rows_per_page, len(classifiers) + 1, i)
+            if name == "data":
+                ax.set_xlabel("".join([each[2],"-",each[3]]),fontsize=16)
+                ax.set_ylabel("".join([each[0],"-",each[1]]),fontsize=16)
 
-            # Get the model and score
-            data = trained_models['fits'].loc[color_cut,name]
-            #fit = pickle.loads(data.trained)
-            score = trained_models['scores'].loc[color_cut,name]
-
-            # Grab the data points
-            # -- Should the same for every color cut, so this is kind of
-            #    repetitive. May change in the future
-            X_train = data.X_train
-            y_train = data.y_train
-            X_test = data.X_test
-            y_test = data.y_test
-
-            # Plot the decision boundary. For that, we will assign a color to each
-            # point in the mesh [x_min, m_max]x[y_min, y_max].
-            if hasattr(data.clf, "decision_function"):
-                Z = data.clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
             else:
-                Z = data.clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+                model = pickle.loads(trained_models[name]._trained_models[each])
 
-            # Put the result into a color plot
-            Z = Z.reshape(xx.shape)
-            ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+                if hasattr(model, "decision_function"):
+                    Z = model.decision_function(np.c_[xx.ravel(), yy.ravel()])
+                else:
+                    Z = model.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
 
-            # Plot also the training points
-            ax.scatter(X_train[:, 0], X_train[:, 1], s=20, c=y_train, cmap=cm_bright)
-            # and testing points
-            ax.scatter(X_test[:, 0], X_test[:, 1], s=40, c=y_test, cmap=cm_bright,
-                       alpha=0.6)
+                # Plot the decision boundary. For that, we will assign a color to each
+                # point in the mesh [x_min, m_max]x[y_min, y_max].
+                Z = Z.reshape(xx.shape)
+                ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
 
+                # Get score and label plot
+                score = int(100*model.score(X, y))
+                ax.text(xx.max() - .1, yy.min() + .1, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+
+            ax.scatter(X[:, 0], X[:, 1], s=30, c=y, cmap=cm_bright, alpha=0.6)
             ax.set_xlim(xx.min(), xx.max())
             ax.set_ylim(yy.min(), yy.max())
-            ax.text(xx.max() - .1, yy.min() + .1, ('%.2f' % score).lstrip('0'),
-            size=15, horizontalalignment='right')
-            plt.gcf().subplots_adjust(bottom=0.10,left=0.10,right=0.90,top=0.90)
 
-            # If on the first row of the page, print titles
+            # If the first row of the plot, print the column labels
             if j == 1: ax.set_title(name)
 
+            # If hit maximum pages to plot, save figure and start a new one (page)
             if i == int(rows_per_page*ncols):
                 # Make room for labels
                 # Save the page
@@ -216,12 +176,12 @@ def plot_all_classifiers(filename=None):
                 # Close the figure to save memory
                 plt.close()
                 # Re-initialize the figure
-                figure = plt.figure(figsize=((len(classifiers)+1)*3,rows_per_page*3))
+                figure = plt.figure(figsize=(ncols*3,rows_per_page*3))
                 # Reset plot number and classifier title flag
-                i, j = 1, 0
-            else: i += 1
+                i, j = 1, 1
+            else: i+=1
 
-        # Done with first row, increment so it no longer labels plot
+        # Done with row of colors
         j += 1
 
     # Close pdf
